438d265e23cd5333cfeefa8b2fb0984319f2d079
SwiftFc <aseiduhenrydade@gmail.com>
2023-07-13 19:08:04 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,12 @@
 #include <unistd.h>
 
 /**
- * main - A C program that prints a new line to the standard error
- * Return: 1 (success)
+ *  main - Entry point
+ *
+ *  Return: Always 1 (Success)
  */
-
 int main(void)
 {
-
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
-
-}	
+}
