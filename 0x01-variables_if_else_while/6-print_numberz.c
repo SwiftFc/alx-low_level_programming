13af3ba01237e289d7cc472e0c327b3c7683fcd1
SwiftFc <aseiduhenrydade@gmail.com>
2023-07-14 17:59:20 -0700

    print numbers from 0 to 9

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Print the single digit numbers 
- * starting from 0 to 9.
+ * main - Prints numbers from 0 to 9.
  *
  * Return: Always 0 (Success)
  */
@@ -10,11 +9,10 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i < 58; i++)
 	{
-		printf("%d", i);
+		putchar(i);
 	}
 	putchar('\n');
 	return (0);
 }
-
