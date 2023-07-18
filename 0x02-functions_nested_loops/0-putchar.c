12a4c6d4f788996e753412f7b24973eb3886843b
SwiftFc <aseiduhenrydade@gmail.com>
2023-07-18 11:32:04 -0700

    write a program that prints putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - Print "_putchar" followed by a new line.
- * Description: You are not allowed to include standard libraries.
+ * main - write a program that print _putchar
+ * 
  * Return: 0
  */
 int main(void)
 {
-	char _putchar[] = "_putchar";
-	int i = 0;
-
-	while (_putchar[i] != '\0')
-	{
-		_putchar(_putchar[i]);
-		i++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
