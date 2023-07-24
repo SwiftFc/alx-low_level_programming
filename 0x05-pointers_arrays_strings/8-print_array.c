e8361e7bd84a5b07470727a0ac731305f6ef972d
SwiftFc <aseiduhenrydade@gmail.com>
2023-07-24 14:08:03 -0700

    pointers, arrays and strings

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,10 +12,14 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	i = 0;
+	for (n--; n >= 0; n--, i++)
 	{
 		printf("%d", a[i]);
-		if (i < n - 1)
+		if (n > 0)
+		{
 			printf(", ");
+		}
 	}
+	printf("\n");
 }
