41ee3d4ff177c1388e5bbe17ff10aa99e11548be
SwiftFc <aseiduhenrydade@gmail.com>
2023-07-26 12:47:54 -0700

    write a fuction that concatenates two strings using (strncat)

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncat - a program tah concatenates two strings.
+ * @dest: the first string
+ * @src: the second string
+ * @n: number of byte to concatenate
+ * Return: pointer to the resulting string `dest`
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int x, y;
+
+	for (x = 0; dest[x] != '\0'; x++)
+		continue;
+
+	for (y = 0; src[y] != '\0' && n > 0; y++, n--, x++)
+	{
+		dest[x] = src[y];
+	}
+	return (dest);
+}
