57fa7d11850b6b9292871a32f74fe82442b5a40a
SwiftFc <aseiduhenrydade@gmail.com>
2023-07-26 16:29:23 -0700

    Write a function that encodes a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *leet - encode a string into 1337
+ * @s: string to encode
+ * Return: adress of `s`
+ */
+
+char *leet(char *s)
+{
+	int k;
+	int l;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (k = 0; *(s + k); k++)
+	{
+		for (l = 0; l <= 9; l++)
+		{
+			if (a[l] == s[k])
+				s[k] = b[l];
+		}
+	}
+	return (s);
+}
