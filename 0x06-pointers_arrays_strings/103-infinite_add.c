1b919e2d40225bc56c499a24936988971e451ce8
SwiftFc <aseiduhenrydade@gmail.com>
2023-07-27 08:46:09 -0700

    make changes in task 102-magic file

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * *infinite_add - a program that adds two numbers
+ * @n1: represents the first number to be added
+ * @n2: represents the second number to be added
+ * @r: a buffer the program will use to store the result
+ * @size_r: the size of the buffer
+ * Return: a pointer to the result
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
