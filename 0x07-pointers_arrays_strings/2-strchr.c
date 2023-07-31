b7fe3303df4833fe8a76eb9d22349b8f49245d82
SwiftFc <aseiduhenrydade@gmail.com>
2023-07-31 12:29:22 -0700

    update in task 2 locates a character in strin

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -19,6 +19,8 @@ char *_strchr(char *s, char c)
 		}
 		s++;
 	}
+	if (c == '\0')
+		return (s);
 
 	return (NULL);
 }
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * _strspn - a function that gets the length of a prefix substring
