3b92d40c98e269223db508d2cc0ec610fb69bf2d
SwiftFc <aseiduhenrydade@gmail.com>
2023-08-03 06:06:28 -0700

    solution

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - function that checks for uppercase characters
+ * @c: int type number
+ * Return: 1 if uppercase, otherwise return 0
+ */
+
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+		return (1);
+
+	return (0);
+}
