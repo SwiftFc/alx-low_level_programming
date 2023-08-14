3f7b3cb15e5ee7ca6d0b90844f8cba09042c40a7
SwiftFc <aseiduhenrydade@gmail.com>
2023-08-14 06:39:10 -0700

    solution for task on prepossesor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - a function to print the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
