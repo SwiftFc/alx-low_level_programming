cfe4e98f3bea549c8aff2f5e6a6424b7ba656f74
SwiftFc <aseiduhenrydade@gmail.com>
2023-08-14 07:07:45 -0700

    solution in task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,29 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - basic info
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: The owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
