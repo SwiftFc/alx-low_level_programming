72fe984b1e070517f773a6c4c0305e3648ac9271
SwiftFc <aseiduhenrydade@gmail.com>
2023-08-28 11:22:06 -0700

    creating a function that frees the memory of the current node

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - a function that frees a "listint_t" list
+ * @head: pointer
+ *
+ * Return: Not specified
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while (*head != NULL)
+	{
+		current = *head;
+		*head = (*head)->next;
+		free(current);
+	}
+}
