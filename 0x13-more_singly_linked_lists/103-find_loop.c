a8b0dcf0ac6ac9b63a205665b0d7f031b2e03e22
SwiftFc <aseiduhenrydade@gmail.com>
2023-08-29 06:15:29 -0700

    creating a function that finds the loop in a linked list

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,1 +1,41 @@
 #include "lists.h"
+
+/**
+ * find_listint_loop - a function that finds the loop in a linked list
+ * @head: A pointer to the linked list
+ *
+ * Return: The address of the node where the loop starts,
+ * Or NULL if there is no loop
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow, *fast;
+
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+
+	slow = head->next;
+	fast = (head->next)->next;
+
+	while (fast)
+	{
+		if (slow == fast)
+		{
+			slow = head;
+
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+
+			return (slow);
+		}
+
+		slow = slow->next;
+		fast = (fast->next)->next;
+	}
+
+	return (NULL);
+}
