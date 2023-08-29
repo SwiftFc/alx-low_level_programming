efd59cb1d7fa3b929b43f2e57c1d7dda46598ef1
SwiftFc <aseiduhenrydade@gmail.com>
2023-08-29 05:59:14 -0700

    Corrections

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,6 +1,50 @@
 #include "lists.h"
 
 /**
+ * looped_listint_count- a function to count the looped
+ * "listint_t" linked list
+ * @head: A pointer to linked list
+ * Return: 0
+ */
+
+size_t looped_listint_count(listint_t *head)
+{
+	listint_t *slow, *fast;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	slow = head->next;
+	fast = (head->next)->next;
+
+	while (fast)
+	{
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				nodes++;
+				slow = slow->next;
+				fast = fast->next;
+			}
+
+			slow = slow->next;
+			while (slow != fast)
+			{
+				nodes++;
+				slow = slow->next;
+			}
+			return (nodes);
+		}
+		slow = slow->next;
+		fast = (fast->next)->next;
+	}
+	return (0);
+}
+
+/**
  * free_listint_safe - a function that frees a "listint_t" list
  * and set the head to null
  * @h: A double pointer to head linked list
@@ -10,22 +54,31 @@
 
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *current, *hold;
-	size_t count;
+	listint_t *tmp;
+	size_t nodes, index;
 
-	count = 0;
-	current = *h;
-	while (current != NULL)
+	nodes = looped_listint_count(*h);
+
+	if (nodes == 0)
 	{
-		count++;
-		hold = current;
-		current = current->next;
-		free(hold);
-
-		if (hold < current)
-			break;
+		for (; h != NULL && *h != NULL; nodes++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
 	}
-	*h = NULL;
 
-	return (count);
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
+		*h = NULL;
+	}
+	h = NULL;
+	return (nodes);
 }
