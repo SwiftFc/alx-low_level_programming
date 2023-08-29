00f68500f09f2c33fd8ae072375d3e8348b60a0a
SwiftFc <aseiduhenrydade@gmail.com>
2023-08-29 02:43:41 -0700

    creating a function that reverses a linked list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -10,8 +10,7 @@
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev = NULL;
-	listint_t *current = *head;
-	listint_t *next;
+	listint_t *next, *current = *head;
 
 	while (current != NULL)
 	{
