1c1d87e6a0a7d3460338bc59402cd5388dc8b54a
SwiftFc <aseiduhenrydade@gmail.com>
2023-08-31 06:18:56 -0700

    print a binary representation of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  print_binary - a function that prints the binary representation of a number
+ *  @n: string
+ *
+ *  Return: Not specified
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	putchar((n & 1) + '0');
+}
