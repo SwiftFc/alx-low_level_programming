fe5b4ad99fa88756bd04f8cd99c4bc9da6ac46d0
SwiftFc <aseiduhenrydade@gmail.com>
2023-10-02 06:38:40 -0700

    a function that reads a text file and prints it to the POSIX standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * read_textfile - A propgram that reads a text file,
+ * and prints it to the "POSIX" standard output
+ * @letters: This is the number of letters it should read and print
+ * @filename: A pointer to a string representing the file to read
+ *
+ * Return: The actual number of letters, it could read and print
+ * if file fails to open or read, return 0, if `filename` is NULL return 0
+ * if "write" fails to write the expected amount of bytes, return 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer = malloc(letters);
+	int fd = open(filename, O_RDONLY);
+	ssize_t bytes_read, bytes_written = 0;
+
+	if (filename  == NULL)
+		return (0);
+	if (buffer == NULL)
+		return (0);
+
+	if (fd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	bytes_read = read(fd, buffer, letters);
+
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	free(buffer);
+	close(fd);
+	return (bytes_written);
+}
