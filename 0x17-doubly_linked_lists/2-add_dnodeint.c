b13a73d6854513bebebad320459e7e59c1a811ab
SwiftFc <aseiduhenrydade@gmail.com>
2023-11-09 06:00:58 -0800

    a function that adds a new node at the beginning of a dlistint_t list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,8 +17,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	new_node->n = n;
+	new_node->prev = NULL;
 	new_node->next = *head;
 
+	if (*head != NULL)
+		(*head)->prev = new_node;
 	*head = new_node;
 
 	return (new_node);
