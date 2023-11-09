71cfbfc83bc077d17059bfa006b70400e93e661b
SwiftFc <aseiduhenrydade@gmail.com>
2023-11-09 15:06:23 -0800

    deletes node at a given index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -11,6 +11,34 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	dlistint_t *delete = NULL;
 
+	if (head == NULL || *head == NULL)
+		return (-1);
 
+	delete = *head;
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(delete);
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		return (1);
+	}
+
+	while ((index != 0) && (delete->next != NULL))
+	{
+		index -= 1;
+		delete = delete->next;
+	}
+	if (index == 0)
+	{
+		delete->prev->next = delete->next;
+		if (delete->next != NULL)
+			delete->next->prev = delete->prev;
+		free(delete);
+		return (1);
+	}
+	return (-1);
 }
