2a33ea6d0af7d311472eca7a3f950e86c303b572
SwiftFc <aseiduhenrydade@gmail.com>
2023-11-13 22:00:54 -0800

    operations

---
+++ 0x18-dynamic_libraries/maths.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - add two integers
+ * @a: first
+ * @b: second
+ * Return: add
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two integers
+ * @a: first
+ * @b: second
+ * Return: subtract
+ */
+
+int sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * div - divide two integers
+ * @a: first
+ * @b: second
+ * Return: divide
+ */
+
+int div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * mod - mod two integers
+ * @a: first
+ * @b: second
+ * Return: modulos
+ */
+
+int mod(int a, int b)
+{
+        return (a & b);
+}
+
+/**
+ * mul - multiply tow integers
+ * @a: first
+ * @b: second
+ * Return: multiply
+ */
+
+int mul(int a, int b)
+{
+        return (a * b);
+}
