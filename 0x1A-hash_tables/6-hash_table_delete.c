ec4396b575baee1ace9c1d90ab30e7833b76ea54
SwiftFc <aseiduhenrydade@gmail.com>
2023-12-04 13:26:26 -0800

    new changes committed

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,25 +7,30 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_table_t *head = ht;
-	hash_node_t *node, *tmp;
 	unsigned long int i;
+	hash_node_t *node, *next_node;
 
-	for (i = 0; i < ht->size; i++)
+
+	if (ht == NULL)
+		return;
+
+	i = 0;
+	while (i < ht->size)
 	{
-		if (ht->array[i] != NULL)
+		node = ht->array[i];
+		while (node != NULL)
 		{
-			node = ht->array[i];
-			while (node != NULL)
-			{
-				tmp = node->next;
-				free(node->key);
-				free(node->value);
-				free(node);
-				node = tmp;
-			}
+			next_node = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			if (next_node != NULL)
+				node = next_node;
+			else
+				break;
 		}
+		i++;
 	}
-	free(head->array);
-	free(head);
+	free(ht->array);
+	free(ht);
 }
